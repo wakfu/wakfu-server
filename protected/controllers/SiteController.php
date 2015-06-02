c8b288a772b90fea5b0aa8e6e79d0c7cfc33f929
jianhao.dai <jianhao.dai@fraudmetrix.cn>
2015-06-02 21:49:59 +0800

    error handle

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -10,13 +10,7 @@ class SiteController extends Controller{
     public function actionError(){
         if($error=Yii::app()->errorHandler->error)
         {
-            if(Yii::app()->request->isAjaxRequest){
-                echo $error['message'];
-            }else{
-                $this->render('error', $error);
-            }
-        }else{
-            $this->render('error');
+            echo $error['message'];
         }
     }
 }
