451bef013dc5cfbb43b030c3c8fdb5657d5082ff
jianhao.dai <jianhao.dai@fraudmetrix.cn>
2015-06-06 22:38:13 +0800

    bug fix

--- protected/commands/shell/ss-client.sh
+++ protected/commands/shell/ss-client.sh
@@ -2,7 +2,7 @@
 port=$1;
 mode=$2;
 
-if [ "$mode" -eq "start" ]; then
+if [ "$mode" = "start" ]; then
     ss-local -s 23.252.107.210 -p 443 \
         -b 0.0.0.0 -l $port \
         -k daijianhao \
@@ -11,7 +11,7 @@ if [ "$mode" -eq "start" ]; then
         -f /var/run/shm/ss-$port.pid &
 fi
 
-if [ "$mode" -eq "quit" ]; then
+if [ "$mode" = "quit" ]; then
     pid=$(ps -ax|grep ss-local|grep "$port"|awk '{print $1}');
     if [ -n "$pid" ]; then
         kill $pid;
